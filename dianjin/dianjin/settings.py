0c21ab0851d0e380b6f68a7d3f1c36eb691dccee
liangxiaoguang <liangtian68@outlook.com>
2018-12-29 11:24:04 +0800

    123

--- dianjin/dianjin/settings.py
+++ dianjin/dianjin/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,14 @@ WSGI_APPLICATION = 'dianjin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dianjin',
+        'USER': 'root',
+        'PASSWORD': 'pachong123',
+        'HOST': '47.100.15.193',
+        'PORT': '3306',
     }
 }
 
