e2a4fb52ee2e5fa6520338278b2ce2999dd46295
liangxiaoguang <liangtian68@outlook.com>
2019-01-03 16:47:03 +0800

    123

--- dianjin/dj_app/urls.py
+++ dianjin/dj_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('xinlang', views.xinlang(), name='xinlang'),
+    path('xinlang', views.xinlang, name='xinlang'),
     path('hupu', views.hupu, name='hupu'),
     path('duowan', views.duowan, name='duowan'),
 
