1e85d43c3979503c9270774a0e09f2bc955b2a5b
liangxiaoguang <liangtian68@outlook.com>
2019-01-28 16:01:22 +0800

    123

--- dianjin/scripts/hupu.py
+++ dianjin/scripts/hupu.py
@@ -93,7 +93,7 @@ def inster_content():
 
     obj_list = hupu.objects.filter(content='')
 
-    print(len(obj_list))
+    #print(len(obj_list))
     for obj in obj_list:
 
         result,html = get_content(obj.c_id)
@@ -101,11 +101,17 @@ def inster_content():
         #
 
         try:
-            title = result.find('h1', attrs={'id':'j_data', 'data-maxpage':re.compile('(\d)*')}).text
             print(obj.c_id)
+            #pdb.set_trace()
+            title = result.find('h1', attrs={'id':'j_data', 'data-maxpage':re.compile('(\d)*')}).text
+
         except AttributeError:
-            #在数据库中删除该条数据
-            hupu.objects.filter(c_id=obj.c_id).delete()
+
+            try:
+                #在数据库中删除该条数据
+                hupu.objects.filter(c_id=obj.c_id).delete()
+            except:
+                continue
 
             continue
             # 时间
@@ -137,7 +143,27 @@ def inster_content():
         #         print(url)
         #         img_list[index] = img_list = selector.xpath(url)[0]
 
-        # print(b)
+
+
+        #第二种情况img_list没有取到数据
+        if img_list == []:
+            img_list = selector.xpath('//div[@class="quote-content"]//div//idiv//@src')
+
+        #b = '<img src="https://i1.hoopchina.com.cn/blogfile/201901/07/BbsImg_53468974497719_1546864084_s_24009_o_w_250_h_375_12658.png?x-oss-process=image/resize,w_800/format,webp"'
+        #print(re.findall('<img src="(.*)" ', b))
+        l1 = []
+
+        l1 = re.findall('(?<![video|script]) src="([^"]*)"', b)
+        # 和下面的对应的
+        # l1 = re.findall('<img src="([^"]*)" data-original="[^"]*"', b)
+        l2=re.findall('(?<![video|script]) src="[^"]*" data-original="([^"]*)"', b)
+
+
+        print(len(l1),len(l2),len(img_list))
+        #测试用
+        if len(img_list)>len(l1):
+            print("_______________________________")
+            return
 
         #print(len(img_list))
         if len(data_list) !=0:
@@ -165,7 +191,7 @@ def inster_content():
 
 
             b = b.replace(imgurl,'http://47.100.15.193/hupu/{}_{}.jpg'.format(id,index_),2)
-        #print(b)
+        #print(img_list)
         obj.content = b
         obj.c_title = title
         obj.c_time = ctime
@@ -173,6 +199,7 @@ def inster_content():
             obj.save()
         except:
             continue
+
 
 def get_hupu(ps,pn):
 
